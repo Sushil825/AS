7ace1bec040e717a4796c1a711dd43057fcf37d4
Abhishek <7005093380a@gmail.com>
2025-07-17 21:57:53 +0530

    Completed all the enemies

--- Scenes/Entities/Enemies/small_bee.gd
+++ Scenes/Entities/Enemies/small_bee.gd
@@ -2,26 +2,27 @@ extends CharacterBody2D
 
 enum EnemyState
 {
-	RUN,
-	WALK,
-	DEATH,
-	IDLE,
+	FLY,
 	TAKEDAMAGE,
+	ATTACK,
+	DEATH,
 }
 
-var current_state: EnemyState = EnemyState.WALK
+var current_state: EnemyState = EnemyState.FLY
 var Game_State : bool = true
 var player : CharacterBody2D = null
 var direction : Vector2 = [Vector2.RIGHT, Vector2.LEFT].pick_random()
 var gotAttacked : bool = false
+var canAttack : bool = false
 
-@export var gravity : int = 800
 @export var Health : float = 100
 @export var speed : int = 50
 
 @onready var Animations : AnimatedSprite2D = $AnimatedSprite2D
 @onready var movement_timer: Timer = $"Movement Timer"
+@onready var attack_delay : Timer = $"Attack Delay"
 @onready var detection_area: CollisionShape2D = $"Detection Area/CollisionShape2D"
+@onready var hurt_box_component: HurtBoxComponent = $HurtBoxComponent
 
 func OnAttack() -> void:
 	pass
@@ -31,32 +32,23 @@ func Take_Damage(Power : float) -> void:
 	gotAttacked = true
 		
 func Game_Loop() -> void:
+	randomize()
 	
 	if Health <= 0:
 		current_state = EnemyState.DEATH
 	
 	match current_state:
-		EnemyState.DEATH:
-			direction = Vector2.ZERO
-			Animations.play("hit")
-			await Animations.animation_finished
-			call_deferred("queue_free")
-		
-		EnemyState.RUN:
-			speed = 100
+		EnemyState.FLY:
+			Animations.play("fly")
 			if player:
-				direction.x = -1 * (player.global_position - self.global_position).normalized().x
-			direction.x = round(direction.x)
-			direction.y = 0
-			Animations.play("run")
-			current_state = EnemyState.WALK if !player else current_state
-			current_state = EnemyState.TAKEDAMAGE if gotAttacked else current_state
-		
-		EnemyState.IDLE:
-			direction = Vector2.ZERO
-			Animations.play("idle")
-			await Animations.animation_finished
-			current_state = EnemyState.RUN if player else EnemyState.WALK
+				direction.x = round((player.global_position - self.global_position).normalized().x)
+				direction.y = 0
+			else:
+				if movement_timer.is_stopped():
+					direction = [Vector2.LEFT, Vector2.RIGHT].pick_random()
+					movement_timer.wait_time = randf_range(2.5,10)
+					movement_timer.start()
+			current_state = EnemyState.ATTACK if canAttack else current_state
 			current_state = EnemyState.TAKEDAMAGE if gotAttacked else current_state
 		
 		EnemyState.TAKEDAMAGE:
@@ -65,18 +57,23 @@ func Game_Loop() -> void:
 			await get_tree().create_timer(1).timeout
 			Animations.play("hit")
 			await Animations.animation_finished
-			current_state = EnemyState.IDLE
+			current_state = EnemyState.FLY
 		
-		EnemyState.WALK:
-			randomize()
-			speed = 50
-			if movement_timer.is_stopped():
-				direction = [Vector2.LEFT, Vector2.RIGHT].pick_random()
-				movement_timer.wait_time = randf_range(2,6)
-				movement_timer.start()
-			Animations.play("walk")
-			current_state = EnemyState.RUN if player else current_state
-			current_state = EnemyState.TAKEDAMAGE if gotAttacked else current_state
+		EnemyState.ATTACK:
+			if attack_delay.is_stopped():
+				direction = Vector2.ZERO
+				Animations.play("attack")
+				await Animations.animation_finished
+				attack_delay.wait_time = randf_range(2,5)
+				attack_delay.start()
+			else:
+				current_state = EnemyState.FLY
+			
+		EnemyState.DEATH:
+			direction = Vector2.ZERO
+			Animations.play("hit")
+			await Animations.animation_finished
+			call_deferred("queue_free")
 			
 	Game_State = true
 
@@ -92,10 +89,19 @@ func _ready() -> void:
 		func(_body: CharacterBody2D):
 			player = null
 	)
+	
+	hurt_box_component.area_entered.connect(
+		func(_area: Area2D):
+			canAttack = true
+	)
+	
+	hurt_box_component.area_exited.connect(
+		func(_area: Area2D):
+			canAttack = false
+	)
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	velocity = direction * speed
-	velocity.y += gravity
 	move_and_slide()
 	
 	if velocity.normalized().x > 0:
